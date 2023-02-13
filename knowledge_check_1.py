92c6a91a7536eadc82603859549160427e2a02b9
Jamie Smither <smither.jamie@gmail.com>
2023-02-12 19:45:08 -0500

    adding knowledge check 1

---
+++ knowledge_check_1.py
@@ -0,0 +1,20 @@
+#a statement that prints Hello World!
+print ('hello world!')
+
+#a list populated with several values
+my_list = ["code", "louisville", "is", "awesome", "thanks", "for", "being", "mentors"]
+
+#print a value from the list
+print (my_list[1])
+
+#a dictionary with two keys and two values
+my_dict = {"Apples":3.50, "Oranges":2.50, "Pears":5.00}
+
+#print a dictionary value
+print(my_dict["Apples"])
+
+#a tuple with 4 values
+my_tuple = (3,8,15,6)
+
+#print a value from the tuple
+print(my_tuple[2])
